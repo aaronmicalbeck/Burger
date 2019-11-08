fb9f6b285d4c074c99444413a4cd576ac679a254
Aaron Beck <aaronmicalbeck@gmail.com>
2019-11-07 18:12:41 -0700

    skeleton

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,32 @@
 const connection = require("../config/connection.js");
 
-selectAll();
-insertOne();
-updateOne();
+const orm = {
+    selectAll : function(tableInput){
+        let queryString = `SELECT * FROM ??`;
+        connection.query(queryString, [tableInput], function(err,result){
+          if (err) throw err;
+          console.log(result);
+        })
+    
+      },
+
+    insertOne : function(tableInput){
+        let queryString = `UPDATE  `;
+        connection.query(queryString, [tableInput], function(err,result){
+          if (err) throw err;
+          console.log(result);
+        })
+    
+      },
+    
+    
+
+
+
+
+}
+
+// insertOne();
+// updateOne();
 
 module.exports = orm;
